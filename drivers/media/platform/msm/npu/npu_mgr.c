351f2d032c46bada996b190243794264eab923fa
Gao Wang <quic_gaowang@quicinc.com>
2024-12-23 17:14:59 +0800

    msm: npu: Fix use after free issue
    There is possibility that network will be used after free.
    This change is to fix this issue.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2025-21424
    CRs-Fixed: 3989614
    
    Change-Id: I12205b750450bee36f85dff3f620f8f0689a4e46
    Signed-off-by: Gao Wang <quic_gaowang@quicinc.com>
    Signed-off-by: Vijeth Kumar K <vijethk@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3219256
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Sipra Patel <siprap@motorola.com>
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/npu/npu_mgr.c
+++ drivers/media/platform/msm/npu/npu_mgr.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-only
+	// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
