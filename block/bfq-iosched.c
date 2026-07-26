7538081f52d043d6827808d2d86d46634cbedd5c
Paolo Valente <paolo.valente@linaro.org>
2026-07-26 14:51:20 +0000

    block, bfq: fix weight-raising resume and soft-rt switching
    Backport of upstream commits:
    56f91ede4d3f - block, bfq: fix weight-raising resume with !low_latency
    9994b0ac0621 - block, bfq: fix switch back from soft-rt weitgh-raising
    
    Reason:
    1) When io_latency heuristic is off, bfq_queues should not start to be
    weight-raised. However, this could happen when the state of a
    previously weight-raised bfq_queue is resumed after a queue split.
    
    2) A bfq_queue may be deemed as soft real-time while still enjoying
    interactive weight-raising. When it loses soft-rt status, it should
    get back its interactive weight-raising if the interactive period is
    not over. This case was not handled.
    
    Backport notes:
    - bfq_bfqq_resume_state: init old_wr_coeff to 1, only restore wr_coeff
      when low_latency is on
    - bfq_update_io_seektime: when ending soft-rt wr during interactive
      period, switch back to interactive wr instead of just ending wr
    - Functions and code paths match upstream
    
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -1019,7 +1019,7 @@ static void
 bfq_bfqq_resume_state(struct bfq_queue *bfqq, struct bfq_data *bfqd,
 		      struct bfq_io_cq *bic, bool bfq_already_existing)
 {
-	unsigned int old_wr_coeff = bfqq->wr_coeff;
+	unsigned int old_wr_coeff = 1;
 	bool busy = bfq_already_existing && bfq_bfqq_busy(bfqq);
 
 	if (bic->saved_has_short_ttime)
@@ -1034,7 +1034,13 @@ bfq_bfqq_resume_state(struct bfq_queue *bfqq, struct bfq_data *bfqd,
 
 	bfqq->entity.new_weight = bic->saved_weight;
 	bfqq->ttime = bic->saved_ttime;
-	bfqq->wr_coeff = bic->saved_wr_coeff;
+	/*
+	 * Restore weight coefficient only if low_latency is on
+	 */
+	if (bfqd->low_latency) {
+		old_wr_coeff = bfqq->wr_coeff;
+		bfqq->wr_coeff = bic->saved_wr_coeff;
+	}
 	bfqq->wr_start_at_switch_to_srt = bic->saved_wr_start_at_switch_to_srt;
 	bfqq->last_wr_start_finish = bic->saved_last_wr_start_finish;
 	bfqq->wr_cur_max_time = bic->saved_wr_cur_max_time;
@@ -5240,8 +5246,26 @@ bfq_update_io_seektime(struct bfq_data *bfqd, struct bfq_queue *bfqq,
 
 	if (bfqq->wr_coeff > 1 &&
 	    bfqq->wr_cur_max_time == bfqd->bfq_wr_rt_max_time &&
-	    BFQQ_TOTALLY_SEEKY(bfqq))
-		bfq_bfqq_end_wr(bfqq);
+	    BFQQ_TOTALLY_SEEKY(bfqq)) {
+		if (time_is_before_jiffies(bfqq->wr_start_at_switch_to_srt +
+					   bfq_wr_duration(bfqd))) {
+			/*
+			 * In soft_rt weight raising with the
+			 * interactive-weight-raising period
+			 * elapsed (so no switch back to
+			 * interactive weight raising).
+			 */
+			bfq_bfqq_end_wr(bfqq);
+		} else { /*
+			  * stopping soft_rt weight raising
+			  * while still in interactive period,
+			  * switch back to interactive weight
+			  * raising
+			  */
+			switch_back_to_interactive_wr(bfqq, bfqd);
+			bfqq->entity.prio_changed = 1;
+		}
+	}
 }
 
 static void bfq_update_has_short_ttime(struct bfq_data *bfqd,
